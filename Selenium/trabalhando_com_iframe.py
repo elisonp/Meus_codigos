ad505046a9ed5fdeed51b909459a809049b42452
elisonp <elisonpatricio@gmail.com>
2022-12-09 12:00:04 -0300

    Comentado para o cenário de mais de um iframe

--- Selenium/trabalhando_com_iframe.py
+++ Selenium/trabalhando_com_iframe.py
@@ -1,8 +1,19 @@
+
+
 # Pega o XPath do iframe e atribui a uma variável
-iframe = driver.find_element_by_xpath("//*[@id="editor"]/div[3]/div[3]/iframe")
+iframe_1 = driver.find_element("xpath","/html/body/div[5]/div[2]/iframe")
 
 # Muda o foco para o iframe
-driver.switch_to.frame(iframe)
+driver.switch_to.frame(iframe_1)
 
 # Retorna para a janela principal (fora do iframe)
 driver.switch_to.default_content()
+
+
+#       ATENÇÃO
+'''
+    Caso encontre o cenário que tem mais de um iframe, você deverá
+voltar a janela principal, com o comando da linha 10 e então criar
+uma outra variável como o da linha 4 e então mover o foco para o novo
+iframe criado, e alterando para ele como na linha 7.
+'''
