528b613b4ca9061335ceb7d65d598ba83e7dd3a3
elisonp <elisonpatricio@gmail.com>
2022-12-19 15:10:23 -0300

    tipo de arquivo alterado txt > sql e modificado a forma dos comentários

--- sql/condicionais.sql
+++ sql/condicionais.sql
@@ -1,9 +1,9 @@
-# Seleciona a nota
-# Caso quando a nota for 11111, retorne o valor 'one' na coluna CASE
-# caso seja 2, retorne two
-# Se não atenteder nenhuma das condições, retorne other
-# fim
-
+/* Seleciona a nota
+ Caso quando a nota for 11111, retorne o valor 'one' na coluna CASE
+ caso seja 2, retorne two
+ Se não atenteder nenhuma das condições, retorne other
+ fim
+*/
 SELECT nota,
        CASE WHEN nota ='11111' THEN 'one'
             WHEN nota ='2' THEN 'two'
@@ -11,18 +11,16 @@ SELECT nota,
        END
     FROM v360.notas_fiscais nf ;
 
-
-
-# Subquery
-# Retorna Uma query, contanto que re
+ -- Subquery
+ -- Retorna Uma query, contanto que re
  select * 
  from v360.notas_fiscais nf 
  where exists (select * from v360.notas_fiscais_1 nf2  where nota = '11111')
 
 
 
-# Comparar se um valor não existe, caso exista, ele não fará nada,
-# Caso não tenha o valor, ele irá inserir um nova linha
+-- Comparar se um valor não existe, caso exista, ele não fará nada,
+-- Caso não tenha o valor, ele irá inserir um nova linha
 
 DO
 $do$
